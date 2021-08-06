427c9ecbb94af7a776bafd71795361688bd00d7e
Pravin Kunnure <pravinkunnure9@gmail.com>
2021-08-06 18:29:03 +0530

    Reading input using JOnptionPlane

---
+++ src/com/example/read/joptionpane/ReadJOptionPane.java
@@ -0,0 +1,34 @@
+package com.example.read.joptionpane;
+
+import javax.swing.*;
+
+public class ReadJOptionPane
+{
+    public static void main(String[] args)
+    {
+        /**
+         *
+         */
+        String name;
+        String surname;
+
+        name= JOptionPane.showInputDialog("Please enter your name");
+        surname=JOptionPane.showInputDialog("Please enter your surname");
+
+        JOptionPane.showMessageDialog(null,name+" "+surname);
+
+        int firstNumber;
+        int secondNumber;
+        String input;
+
+        input=JOptionPane.showInputDialog("Please enter first number");
+        firstNumber=Integer.parseInt(input);
+
+        input=JOptionPane.showInputDialog("Please enter second number");
+        secondNumber=Integer.parseInt(input);
+
+        double average=(firstNumber+secondNumber)/2.0;
+
+        JOptionPane.showMessageDialog(null,"Average"+average);
+    }
+}
